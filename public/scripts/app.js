0dca4127458797e98cdf0c09dd86ec3c71fb9bca
betttyquu <absheng947@hotmail.com>
2019-05-17 00:13:03 +0000

    adds screenshots & minor display fixes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,54 +1,55 @@
-
+// appending each tweet element to html
 function createTweetElement(tweet) {
   let $tweet = $('<article>').addClass('tweet');
   let $header = $('<header>');
   let $footer = $('<footer>');
   let $image = $('<img>').attr('id', 'avatar').attr('src', tweet.user.avatars["small"]);
-  let $icon = $('<img>').attr('id', 'icon').attr('src',"http://chittagongit.com/images/tweet-icon-png/tweet-icon-png-17.jpg");
-  // let time = tweet["created_at"];
+  let $icon = $('<img>').attr('id', 'icon').attr('src', "http://chittagongit.com/images/tweet-icon-png/tweet-icon-png-17.jpg");
   $header.append($image);
   $footer.append($icon);
   $header.append('<h3>' + tweet.user["name"] + '</h3>');
-  $header.append('<h4>' + tweet.user["handle"]+ '</h4>');
+  $header.append('<h4>' + tweet.user["handle"] + '</h4>');
   $footer.append('<h5>' + moment(new Date(tweet["created_at"])).fromNow() + '</h5>');
   $tweet.append($header);
   $tweet.append('<p>' + tweet.content["text"]);
   $tweet.append($footer);
   return $tweet;
 }
-  
+
+
 function renderTweets(tweetArray) {
   $('.tweets-container').empty();
   tweetArray.forEach(data => {
     $('.tweets-container').prepend(createTweetElement(data));
   })
 };
-// renderTweets(data);
 
-$( "#newpost" ).on('submit',(function(event) {
+// when clicked tweet;
+$("#newpost").on('submit', (function (event) {
   event.preventDefault();
-  const str = $( "form" ).serialize();
+  const str = $("form").serialize();
   const text = document.querySelector("textarea");
   const empty = "";
-
-  if(text.value && text.value.length < 140){
-    $.post( "http://localhost:8080/tweets",str, function(data) {
+  if (text.value && text.value.length < 140) {
+    $.post("http://localhost:8080/tweets", str, function (data) {
       loadTweets();
+      $('#textinput').val('');
+      $('span.counter').text(140);
       $('.error').hide();
     });
-  } if (text.value === empty){
+  } if (text.value === empty) {
     $("#emptyError").slideDown();
 
-  } if(text.value.length > 140){
+  } if (text.value.length > 140) {
     $("#maxCharactersError").slideDown();
   }
 }));
 
 //success callback func = renderTweets();
-function loadTweets (){
-  $.get('/tweets',function(data){
+function loadTweets() {
+  $.get('/tweets', function (data) {
     renderTweets(data);
-  } 
+  }
 )}
 
 $(function() {
@@ -56,29 +57,33 @@ $(function() {
 });
 
 // compose button toggles textarea
-$("#compose").click(function() {
+$("#compose").click(function () {
   $(".new-tweet").slideToggle();
 });
 
 // directly go to textarea w/o clicking it
-$( "#compose" ).click(function() {
-  $( "textarea" ).focus();
+$("#compose").click(function () {
+  $("textarea").focus();
 });
 
+//textbox hidden on page load
+$(".new-tweet").hide();
+
 // hide error
-$( ".error" ).hide();
+$(".error").hide();
 
 // error message displayed when there's error, slides away when there isn't
 $("#textinput").on('input',
-function(){
-  const text = document.querySelector("textarea");
-  const empty = "";
-  if(text.value.length === empty) {
-    $("#emptyError").slideDown();
-  }
-  if(text.value.length > 140) {
-    $("#maxCharactersError").slideDown();
-  } else {
-    $('.error').hide();
-  }
-});
+  function () {
+    const text = document.querySelector("textarea");
+    const empty = "";
+    if (text.value.length === empty) {
+      $("#emptyError").slideDown();
+    }
+    if (text.value.length > 140) {
+      $("#maxCharactersError").slideDown();
+    } else {
+      $('.error').hide();
+    }
+  });
+
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,16 +1,13 @@
 "use strict";
-// Simulates the kind of delay we see with network or filesystem operations
-
 
 module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
-    saveTweet: function saveTweet(newTweet, callback){
+    saveTweet: function saveTweet(newTweet, callback) {
       db.collection("tweets").insertOne(newTweet);
       callback(null, true);
     },
-
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function getTweets(callback) {
@@ -20,4 +17,3 @@ module.exports = function makeDataHelpers(db) {
   };
 }
 
-// Defines helper functions for saving and getting tweets, using the database `db`
